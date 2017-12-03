44115d690b394c061929bc52fb4b10faf83e5d16
telin0411 <eggnome0411@gmail.com>
2017-12-02 19:38:53 -0800

    added new model for cifar10

--- network.py
+++ network.py
@@ -31,7 +31,8 @@ class _netG(nn.Module):
         # Transposed Convolution 5
         self.tconv5 = nn.Sequential(
             nn.ConvTranspose2d(192, 64, 5, 2, 0, bias=False),
-            nn.Tanh(),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
         )
         # Transposed Convolution 5
         self.tconv6 = nn.Sequential(
@@ -143,9 +144,9 @@ class _netD(nn.Module):
         return realfake, classes
 
 
-class _netG_CIFAR10(nn.Module):
+class _netG_CIFAR10_old(nn.Module):
     def __init__(self, ngpu, nz):
-        super(_netG_CIFAR10, self).__init__()
+        super(_netG_CIFAR10_old, self).__init__()
         self.ngpu = ngpu
         self.nz = nz
 
@@ -165,7 +166,7 @@ class _netG_CIFAR10(nn.Module):
         )
         # Transposed Convolution 4
         self.tconv4 = nn.Sequential(
-            nn.ConvTranspose2d(96, 3, 5, 2, 0, bias=False),
+            nn.ConvTranspose2d(96, 3, 8, 2, 0, bias=False),
             nn.Tanh(),
         )
 
@@ -186,6 +187,73 @@ class _netG_CIFAR10(nn.Module):
             tconv3 = self.tconv3(tconv2)
             tconv4 = self.tconv4(tconv3)
             output = tconv4
+        return output
+
+
+class _netG_CIFAR10(nn.Module):
+    def __init__(self, ngpu, nz):
+        super(_netG_CIFAR10, self).__init__()
+        self.ngpu = ngpu
+        self.nz = nz
+
+        # first linear layer
+        self.fc1 = nn.Linear(110, 384)
+        # Transposed Convolution 2
+        self.tconv2 = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode='bilinear'),
+            nn.Conv2d(384, 192, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(192),
+            nn.ReLU(True),
+        )
+        # Transposed Convolution 3
+        self.tconv3 = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode='bilinear'),
+            nn.Conv2d(192, 96, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(96),
+            nn.ReLU(True),
+        )
+        # Transposed Convolution 4
+        self.tconv4 = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode='bilinear'),
+            nn.Conv2d(96, 48, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(48),
+            nn.ReLU(True),
+        )
+        # Transposed Convolution 5
+        self.tconv5 = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode='bilinear'),
+            nn.Conv2d(48, 32, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(True),
+        )
+        # Transposed Convolution 6
+        self.tconv6 = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode='bilinear'),
+            nn.Conv2d(32, 3, 3, 1, 1, bias=False),
+            nn.Tanh(),
+        )
+
+    def forward(self, input):
+        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            input = input.view(-1, self.nz)
+            fc1    = nn.parallel.data_parallel(self.fc1, input, range(self.ngpu))
+            fc1 = fc1.view(-1, 384, 1, 1)
+            tconv2 = nn.parallel.data_parallel(self.tconv2, fc1, range(self.ngpu))
+            tconv3 = nn.parallel.data_parallel(self.tconv3, tconv2, range(self.ngpu))
+            tconv4 = nn.parallel.data_parallel(self.tconv4, tconv3, range(self.ngpu))
+            tconv5 = nn.parallel.data_parallel(self.tconv5, tconv4, range(self.ngpu))
+            tconv6 = nn.parallel.data_parallel(self.tconv6, tconv5, range(self.ngpu))
+            output = tconv6
+        else:
+            input = input.view(-1, self.nz)
+            fc1    = self.fc1(input)
+            fc1 = fc1.view(-1, 384, 1, 1)
+            tconv2 = self.tconv2(fc1)
+            tconv3 = self.tconv3(tconv2)
+            tconv4 = self.tconv4(tconv3)
+            tconv5 = self.tconv5(tconv4)
+            tconv6 = self.tconv6(tconv5)
+            output = tconv6
         return output
 
 
